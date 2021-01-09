7fa3c301d219a4be8b10ec30ac3313883feae9a5
pkentaur <n.smolianitski@gmail.com>
2021-01-09 12:40:09 +0300

    phpmyadmin is working

--- zmkube_restart.sh
+++ zmkube_restart.sh
@@ -3,7 +3,7 @@
 minikube delete
 minikube start
 eval $(minikube docker-env)
-docker build -t nginx_img .
+#docker build -t nginx_img .
 minikube addons enable metallb
 kubectl apply -f srcs/loadbalancer.yaml
-kubectl apply -f srcs/nginx/nginx.yaml
+#kubectl apply -f srcs/nginx/nginx.yaml
