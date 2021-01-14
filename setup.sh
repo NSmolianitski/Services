964fa5882a919f34262f28afc82380a8123b6657
pkentaur <n.smolianitski@gmail.com>
2021-01-14 13:41:21 +0300

    VSFTPD now dies correctly (with pod)

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 minikube delete
 
-minikube --vm-driver=virtualbox start --cpus 4 --memory 4000
+minikube --vm-driver=virtualbox start --cpus 2 --memory 4000
 eval $(minikube docker-env)
 minikube addons enable metallb
 
@@ -15,7 +15,6 @@ docker build -t wordpress_img srcs/wordpress/.
 docker build -t ftps_img srcs/ftps/.
 
 kubectl apply -f srcs/loadbalancer.yaml
-kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/influxdb/influxdb.yaml
 kubectl apply -f srcs/grafana/grafana.yaml
 kubectl apply -f srcs/mysql/mysql.yaml
@@ -23,6 +22,5 @@ sleep 3
 kubectl exec $(kubectl get pods | grep mysql | awk '{print $1}') -- /bin/sh -c /scripts/restore_database.sh
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/ftps/ftps.yaml
-
-minikube dashboard &
